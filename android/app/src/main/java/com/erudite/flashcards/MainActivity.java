8d023cbd378a2a75141071e0c0875c533be73a16
EruditeCoder108 <muktij950@gmail.com>
2026-06-20 17:39:04 +0530

    fixed the black box problem

--- android/app/src/main/java/com/erudite/flashcards/MainActivity.java
+++ android/app/src/main/java/com/erudite/flashcards/MainActivity.java
@@ -1,7 +1,11 @@
 package com.erudite.flashcards;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -9,5 +13,20 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
+
+        // Fix system text-selection toolbar (Cut/Copy/Paste) colors
+        // by disabling WebView algorithmic darkening
+        try {
+            WebView webView = getBridge().getWebView();
+            if (webView != null) {
+                webView.setForceDarkAllowed(false);
+
+                if (WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING)) {
+                    WebSettingsCompat.setAlgorithmicDarkeningAllowed(webView.getSettings(), false);
+                }
+            }
+        } catch (Exception e) {
+            // Non-critical: if WebView isn't ready yet, the theme fix alone should help
+        }
     }
 }
